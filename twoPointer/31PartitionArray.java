92be5df512db124751c9dac51cc296216b9482fb
Tang, Kai <kai_tang@manulife.com>
2020-09-24 11:48:44 -0400

    31

---
+++ twoPointer/31PartitionArray.java
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param nums: The integer array you should partition
+     * @param k: An integer
+     * @return: The index after partition
+     */
+    public int partitionArray(int[] nums, int k) {
+        // write your code here
+
+        int left = 0;
+        int right = nums.length-1;
+
+        while(left<right){
+
+            while(left<right && nums[left] <k){
+                left++;
+            }
+
+            while(left<right && nums[right] >= k){
+                right--;
+            }
+
+            if(left<right){
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+
+            }
+        }
+
+        if(right== nums.length-1){
+            return nums.length;
+        } else if(left ==0){
+            return 0;
+        }else{
+            return left;
+
+        }
+
+
+    }
+}
