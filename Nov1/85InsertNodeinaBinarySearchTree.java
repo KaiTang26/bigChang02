6cc97f2d7ba6e5db78683af0049164c632ac2d9b
Kai Tang <16kt16@queensu.ca>
2020-10-31 22:01:27 -0400

    85

---
+++ Nov1/85InsertNodeinaBinarySearchTree.java
@@ -0,0 +1,28 @@
+
+
+public class Solution {
+    /*
+     * @param root: The root of the binary search tree.
+     * @param node: insert this node into the binary search tree
+     * @return: The root of the new binary search tree.
+     */
+    public TreeNode insertNode(TreeNode root, TreeNode node) {
+        // write your code here
+
+        if(root==null){
+            return node;
+        }
+
+    
+
+        if(node.val>root.val){
+            root.right = insertNode(root.right, node);
+        }
+
+        if(node.val<root.val){
+            root.left = insertNode(root.left, node);
+        }
+
+        return root;
+    }
+}
