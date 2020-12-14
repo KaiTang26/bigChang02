91936cb3b9809e78a3beca3047cbe06a5499c89c
Kai Tang <16kt16@queensu.ca>
2020-12-13 21:46:47 -0500

    8

---
+++ Dec13/8RotateString.java
@@ -0,0 +1,26 @@
+public class Solution {
+    /**
+     * @param str: An array of char
+     * @param offset: An integer
+     * @return: nothing
+     */
+    public void rotateString(char[] str, int offset) {
+        // write your code here
+        revoice(str, 0, str.length-1);
+
+        revoice(str, 0, offset-1);
+
+        revoice(str, offset, str.length-1);
+    }
+
+    public void revoice(char[] str, int start, int end){
+
+
+        while(start<end){
+
+            char temp = str[end];
+            str[end] = str[start];
+            str[start] = temp;
+        }
+    }
+}
