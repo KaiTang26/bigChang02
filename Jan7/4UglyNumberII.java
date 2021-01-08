4df41b42d2726f7e122851cc52afb460522f87fd
Kai Tang <16kt16@queensu.ca>
2021-01-07 19:49:26 -0500

    4

---
+++ Jan7/4UglyNumberII.java
@@ -0,0 +1,43 @@
+import java.util.Queue;
+
+public class Solution {
+    /**
+     * @param n: An integer
+     * @return: return a  integer as description.
+     */
+    public int nthUglyNumber(int n) {
+        // write your code here
+
+        Queue<Long> queue = new PriorityQueue<Long>();
+        Set<Long> set = new HashSet<Long>();
+
+        Long[] primes = new Long[]{Long.valueOf(2),Long.valueOf(3),Long.valueOf(5)};
+
+        for(int i=0; i<3; i++){
+            queue.add(primes[i]);
+            set.add(primes[i]);
+        }
+
+        Long number = Long.valueOf(1);
+
+        for(int i=1; i<n; i++){
+
+            number = queue.poll();
+
+            for(int j=0; j<3; j++){
+
+                if(set.add(number*primes[j])){
+                    queue.add(number*primes[j]);
+                }
+
+            }
+
+        }
+
+        return number.intValue();
+
+
+
+
+    }
+}
