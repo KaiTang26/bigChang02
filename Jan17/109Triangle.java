733cd6cfcaf665fcdfa0b482f944da4e00afb4eb
Kai Tang <16kt16@queensu.ca>
2021-01-16 19:54:44 -0500

    109

---
+++ Jan17/109Triangle.java
@@ -0,0 +1,108 @@
+public class Solution_1 {
+    /**
+     * @param triangle: a list of lists of integers
+     * @return: An integer, minimum path sum
+     */
+    public int h;
+    public int[][] triangle;
+    public int ans;
+    public int minimumTotal(int[][] triangle) {
+        // write your code here
+
+        this.triangle = triangle;
+
+        this.h = triangle.length;
+        this.ans = Integer.MAX_VALUE;
+
+        traver(0, 0, 0);
+        return this.ans;
+
+    }
+    public void traver(int x, int y, int sum){
+        if(x==this.h){
+            if(sum<this.ans){
+                this.ans=sum;  
+            }
+            return;
+        }
+        traver(x+1, y, sum+this.triangle[x][y]);
+        traver(x+1, y+1, sum+this.triangle[x][y]);
+    }
+}
+
+
+public class Solution_2 {
+    /**
+     * @param triangle: a list of lists of integers
+     * @return: An integer, minimum path sum
+     */
+    public int h;
+    public int[][] triangle;
+
+    public int minimumTotal(int[][] triangle) {
+        // write your code here
+        this.triangle = triangle;
+
+        this.h = triangle.length;
+
+        return helper(0, 0);
+
+        
+
+    }
+
+    public int helper(int x, int y){
+
+        if(x==this.h){
+            return 0;
+        }
+
+        return Math.min(triangle[x][y]+helper(x+1, y), triangle[x][y]+helper(x+1, y+1));
+
+    }
+    
+}
+
+
+public class Solution_2 {
+    /**
+     * @param triangle: a list of lists of integers
+     * @return: An integer, minimum path sum
+     */
+
+    public int h;
+    public int[][] triangle;
+    public int[][] hash;
+   
+    public int minimumTotal(int[][] triangle) {
+
+        this.triangle = triangle;
+
+        this.h = triangle.length;
+
+        this.hash = new int[h][h];
+
+        for(int i=0; i<h; i++){
+            for(int j=0; j<h; j++){
+                this.hash[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        return helper(0,0);
+    }
+
+    public int helper(int x, int y){
+
+        if(x==this.h){
+            return 0;
+        }
+
+        if(this.hash[x][y]!=Integer.MAX_VALUE){
+            return this.hash[x][y];
+        }
+
+        this.hash[x][y] = Math.min(triangle[x][y]+helper(x+1, y), triangle[x][y]+helper(x+1, y+1));
+
+        return this.hash[x][y];
+    }
+}
