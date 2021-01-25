99a61dd955e867c1a901a3738e45632e4e7a8c59
Kai Tang <16kt16@queensu.ca>
2021-01-24 19:42:38 -0500

    111

---
+++ Jan24/111ClimbingStairs.java
@@ -0,0 +1,31 @@
+public class Solution {
+    /**
+     * @param n: An integer
+     * @return: An integer
+     */
+    public int climbStairs(int n) {
+        // write your code here
+        if(n<0){
+            return -1;
+        }
+
+        if(n<=2){
+            return n;
+        }
+
+        //setup state
+        int[] pathCount = new int[n];
+
+        for(int i=0; i<2; i++){
+            pathCount[i]=i+1;
+        }
+
+        for(int i=2; i<n; i++){
+            pathCount[i] = pathCount[i-1]+pathCount[i-2];
+        }
+
+        return pathCount[n];
+
+
+    }
+}
