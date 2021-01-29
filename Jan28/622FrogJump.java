cc90da22c2d5c8d34941ba856027297a4ecffa2c
Kai Tang <16kt16@queensu.ca>
2021-01-28 19:59:27 -0500

    622

--- Jan28/622FrogJump.java
+++ Jan28/622FrogJump.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class Solution {
     /**
      * @param stones: a list of stones' positions in sorted ascending order
@@ -5,5 +7,45 @@ public class Solution {
      */
     public boolean canCross(int[] stones) {
         // write your code here
+
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+
+        int len = stones.length;
+
+        for(int i=0; i<len; i++){
+            map.put(stones[i], new HashSet());
+        }
+
+        map.get(0).add(0);
+
+
+        for(int i=0; i<len; i++){
+
+            for(int key: map.get(stones[i])){
+
+                for(int step=key-1; step<=key+1; step++){
+
+                    int reachablePosition = step+stones[i];
+
+                    if(step>0 && map.containsKey(reachablePosition)){
+                        map.get(reachablePosition).add(step);
+
+                    }
+
+                }
+            }
+
+        }
+
+
+        if(map.get(stones[len-1]).size()>0){
+            return true;
+        }else{
+            return false;
+        }
+
+
+
+
     }
 }
